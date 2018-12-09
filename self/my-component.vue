0cddddaa1f1f28142fc7d6f88a0d8ebe926abf2e
liuguangping94 <840375931@qq.com>
2018-12-09 20:20:46 +0800

    npm init

---
+++ self/my-component.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="hello">hello {{who}}</div>
+</template>
+
+<script type="text/javascript">
+	module.exports = {
+		name:'my',
+		data(){
+			return{
+				who:'world'
+			}
+		}
+	}
+</script>
+<style type="text/css">
+	.hello{
+		background-color: #ffe;
+	}
+</style>
